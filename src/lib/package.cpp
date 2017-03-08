6f644dbd2eab857a1e1b513b20a856a6a76ebcab
Rijn <bxbian951122@gmail.com>
2017-03-07 18:24:05 -0600

    initialize package class

--- src/lib/package.cpp
+++ src/lib/package.cpp
@@ -1,9 +1,33 @@
+#include <string>
 #include <vector>
 
 namespace network {
 
+using std::string;
+
 class Package {
+   public:
+    Package() {
+        size    = 0;
+        content = "";
+    };
 
+    Package( size_t _size, string _content )
+        : size( _size ), content( _content ){};
+
+    ~Package(){};
+
+    size_t decrypt( char* buffer ) {
+        (void)buffer;
+    };
+
+    char* encrypt() {
+        return NULL;
+    };
+
+   private:
+    size_t size;
+    string content;
+    char*  data;
 };
-
 }
