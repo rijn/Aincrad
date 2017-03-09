c8f6737380ba5c014413ac064a00a4dadad1edc2
YZG112358 <yg7@illinois.edu>
2017-03-08 20:05:26 -0600

    package modified

--- src/lib/package.cpp
+++ src/lib/package.cpp
@@ -16,10 +16,31 @@ class Package : public std::enable_shared_from_this<Package> {
 
     ~Package(){};
 
-    size_t decrypt( char* buffer ) {
-        (void)buffer;
-        return 0;
+    // HEADER | SIZE(4 bytes) | CONTENT
+   /*
+    * Get the buffer, recognize the first 'package' and put it into
+    * the package. return the size of package
+    * _buffer: the buffer received
+    * _size: the size of the received buffer
+    */
+
+
+    size_t decrypt( char* _buffer, size_t _size ) {
+        (void)_buffer;
+        int head_tag_size = sizeof(PACKAGE_HEADER) + 4;
+        if (size < head_tag_size){
+        	return 0;
+        }
+        
+        int package_size = *(int*)(buffer + sizeof(PACKAGE_HEADER)); // get the size of package
+
+
+
+        return 0; // buffer size
     };
+
+    
+
 
     Package& encrypt() {
         return *shared_from_this();
@@ -35,7 +56,7 @@ class Package : public std::enable_shared_from_this<Package> {
 
    private:
     string content;
-    size_t size;
+    size_t size; // buffer size
     char*  buffer;
 };
 }
